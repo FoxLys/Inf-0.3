ea9ce68798ffb46b527e69a82fe0c88233f560d4
FoxLys <FoxLys.com@gmail.com>
2025-11-24 19:55:55 +0100

    =

--- Egzamin INF.03 - Sesja Lato 2022 - Arkusz 03/zgloszenie.php
+++ Egzamin INF.03 - Sesja Lato 2022 - Arkusz 03/zgloszenie.php
@@ -1,23 +1,27 @@
 <?php
 $conn = mysqli_connect(
-	localhost,
-	root,
-	wedkarstwo
+	'localhost',
+	'root',
+	'',
+	'wedkarstwo'
 );
 
 if ($conn->connect_error) {
 	die("conn faile" . $conn->connect_error);
 }
-$q = "INSERT INTO `zawody_wedkarskie` (`id`, `Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, '2', '4', '2021-09-28', ' Andrzej Nowak');";
+echo "good";
 // зміні для повончення
 
 $lowisko = $_POST['lowisko'];
 $date = $_POST['data'];
 $sedzia = $_POST["sedzia"];
 
+$q = "INSERT INTO `zawody_wedkarskie` (`id`, `Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, 0, $lowisko, $date, $sedzia);
+";
+mysqli_query($conn, $q);
 
 
 
 
-$conn->close();
+mysqli_close($conn);
 ?>
