add90877629a6df99e7e723301832d98df8c4353
FoxLys <FoxLys.com@gmail.com>
2025-11-24 19:36:34 +0100

    finall shote

--- Egzamin INF.03 - Sesja Lato 2022 - Arkusz 03/zgloszenie.php
+++ Egzamin INF.03 - Sesja Lato 2022 - Arkusz 03/zgloszenie.php
@@ -5,10 +5,13 @@ $conn = mysqli_connect(
 	wedkarstwo
 );
 
-$q = "";
+$q = "INSERT INTO `zawody_wedkarskie` (`id`, `Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, '2', '4', '2021-09-28', ' Andrzej Nowak');";
 // зміні для повончення
-#sdad
-$
+
+$lowisko = $_POST['lowisko'];
+$date = $_POST['data'];
+$sedzia = $_POST["sedzia"];
+
 
 
 
