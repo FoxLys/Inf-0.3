02dbcc5b0f5223131a84462b418d37bf2a598f7e
FoxLys <FoxLys.com@gmail.com>
2025-12-29 06:36:18 +0100

    css 33%

---
+++ Arkusz INF.03-06-24.01-SG/biuro.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./styl9.css">
+	<title>Poznaj Europę</title>
+</head>
+
+<body>
+
+
+	<header>
+		<h1>BIURO PODRÓŻY</h1>
+	</header>
+
+	<main>
+		<div id="left">
+			<h2>Promocje</h2>
+			<table>
+				<tr>
+					<td>Warszawa</td>
+					<td>od 600 zł
+					</td>
+				</tr>
+				<tr>
+					<td>Wenecja</td>
+					<td>od 1200 zł</td>
+				</tr>
+				<tr>
+					<td>Paryż</td>
+					<td>od 1200 zł</td>
+				</tr>
+			</table>
+		</div>
+		<div id="middle">
+			<h2>W tym roku jedziemy do...</h2>
+
+			<!-- script 1  -->
+		</div>
+		<div id="right">
+			<h2>Kontakt</h2>
+			<a href="mailto:e-mail biuro@wycieczki.pl">napisz do nas</a>
+			<p>telefon: 444555666</p>
+		</div>
+
+		<div id="dane">
+			<h3>W poprzednich latach byliśmy...</h3>
+			<ol>
+				<!-- script 2  -->
+				<li></li>
+				<li></li>
+				<li></li>
+			</ol>
+		</div>
+
+	</main>
+
+	<footer>
+		<p>Stronę wykonał:1112223334455</p>
+	</footer>
+
+	<script>
+		setInterval(() => location.reload(), 3000);
+	</script>
+</body>
+
+</html>
