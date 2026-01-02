009b61ae4eca04b8e731e6564d5625b8ced1d5a9
FoxLys <FoxLys.com@gmail.com>
2026-01-02 05:15:27 +0100

    98%

--- Arkusz INF.03-06-24.01-SG/biuro.php
+++ Arkusz INF.03-06-24.01-SG/biuro.php
@@ -4,9 +4,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./styl9.css">
+	<link rel="stylesheet" href="styl9.css">
 	<title>Poznaj Europę</title>
-</head>
+</head> 
 
 <body>
 
