66450cd375f2e448d4c8d99f267ecd4aee998f8b
FoxLys <FoxLys.com@gmail.com>
2026-01-08 06:25:53 +0100

    html 20%

---
+++ Arkusz INF.03-01-23.06-SG/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styl.css">
+	<title>Sklep dla uczniów</title>
+</head>
+
+<body>
+	<header>
+		<h1>Dzisiejsze promocje naszego sklepu</h1>
+	</header>
+	<main>
+		<section id="left">
+			<h2>Taniej o 30%</h2>
+			<ul>
+				<!-- <script></script> -->
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</section>
+		<section id="mid">
+			<h2>Sprawdź cenę</h2>
+			<form action="Post">
+				input:
+			</form>
+		</section id="right">
+		<section>
+
+		</section>
+	</main>
+	<footer>
+
+	</footer>
+</body>
+
+</html>
