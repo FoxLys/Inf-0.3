9eedc5d92472cc7eb37d5b5040a1369d00af9603
FoxLys <FoxLys.com@gmail.com>
2026-01-12 05:42:33 +0100

    100% Arkusz INF.03-01-23.06-SG

--- Arkusz INF.03-01-23.06-SG/index.php
+++ Arkusz INF.03-01-23.06-SG/index.php
@@ -7,6 +7,15 @@
 	<link rel="stylesheet" href="styl.css">
 	<title>Sklep dla uczniów</title>
 </head>
+<!-- сonnection to  php  -->
+<?php
+$conn = new mysqli(
+	'localhost',
+	'root',
+	'',
+	'sklep'
+);
+?>
 
 <body>
 	<header>
@@ -17,26 +26,50 @@
 			<h2>Taniej o 30%</h2>
 			<ul>
 				<!-- <script></script> -->
-				<li></li>
-				<li></li>
-				<li></li>
+				<?php
+				$sql = "SELECT nazwa FROM `towary` WHERE promocja = 1;";
+
+				$result = $conn->query($sql);
+				while ($row = $result->fetch_array()) {
+					echo '<li>' . $row['nazwa'] . '</li>';
+				}
+				?>
 			</ul>
 		</section>
 		<section id="mid">
 			<h2>Sprawdź cenę</h2>
-			<form action="Post">
+			<form method="POST">
 				<select name="range" id="range">
-					<option value="0">Gumka do mazania</option>
-					<option value="1">Cienkopis</option>
-					<option value="2">Pisaki 60 szt.</option>
-					<option value="3">Markery 4 szt.</option>
+					<option value="Gumka do mazania">Gumka do mazania</option>
+					<option value="Cienkopis">Cienkopis</option>
+					<option value="Pisaki 60 szt.">Pisaki 60 szt.</option>
+					<option value="Markery 4 szt.">Markery 4 szt.</option>
 				</select>
 				<!-- script 2 -->
 				<input type="submit" value="SPRAWDŹ">
 			</form>
-			<p id="sctypt2"></p>
-		</section id="right">
-		<aside>
+			<p id="sctypt2">
+				<?php
+				if (isset($_POST['range'])) {
+
+					$nazwa = $_POST['range'];
+
+					$sql1 = "SELECT `cena` FROM `towary` WHERE nazwa LIKE '$nazwa'";
+
+					$result1 = $conn->query($sql1);
+
+					if ($row1 = $result1->fetch_assoc()) {
+						echo "cena regularna: " . $row1['cena'] . "<br>";
+						$cenaPromo = $row1['cena'] * 0.7;
+						echo "cena w promocji 30%: " . $cenaPromo;
+					} else {
+						echo "Nie znaleziono produktu w bazie!";
+					}
+				}
+				?>
+			</p>
+		</section>
+		<aside id="right">
 			<h2>Kontakt</h2>
 			<p>e-mail:<a href="mailto:bok@sklep.pl">bok@sklep.pl</a></p>
 			<img src="promocja.png" alt="promocja">
@@ -45,6 +78,9 @@
 	<footer>
 		<h4>Autor strony: 132151516516</h4>
 	</footer>
+	<?php
+	$conn->close();
+	?>
 </body>
 
 </html>
